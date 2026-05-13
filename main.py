49a6d498e8b950b3b4b129b69b417bc95377acb4
Samuel Berto <samuztberto@gmail.com>
2026-05-13 04:58:42 -0300

    feat: melhora menu principal do sistema

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-from database.conexao import  criar_tabela_pacientes
-from services.paciente_service import cadastrar_paciente
+from database.conexao import criar_tabela_pacientes
+from services.paciente_service import (
+    cadastrar_paciente,
+    listar_pacientes
+)
 
 criar_tabela_pacientes()
 
-print("🏥 MEDFLOW")
-print("Bem-vindo ao sistema de gerenciamento de pacientes!")
-print("1. Cadastrar paciente")
+while True:
+    print("\n🏥 MEDFLOW")
+    print("Bem-vindo ao sistema de gerenciamento de pacientes!")
+    print("1. Cadastrar paciente")
+    print("2. Listar pacientes")
+    print("0. Sair")
 
-opcao = input("Escolha uma opção: ")
+    opcao = input("Escolha uma opção: ")
 
-if opcao == "1":
-    cadastrar_paciente()
+    if opcao == "1":
+        cadastrar_paciente()
+    elif opcao == "2":
+        listar_pacientes()
+
+    elif opcao == "0":
+        print("👋 Encerrando o sistema...")
+        break
+
+    else:
+        print("❌ Opção inválida. Por favor, escolha uma opção válida.")
