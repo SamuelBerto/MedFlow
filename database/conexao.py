dc6c27387196ca3d63dc539fc8b31af2f0fcc2d7
Samuel Berto <samuztberto@gmail.com>
2026-05-13 03:23:00 -0300

    feat: cria tabela de pacientes no bando de dados

--- database/conexao.py
+++ database/conexao.py
@@ -3,3 +3,19 @@ import sqlite3
 def conectar():
     conexao = sqlite3.connect('medflow.db')
     return conexao
+
+def criar_tabela_pacientes():
+    conexao = conectar()
+
+    cursor = conexao.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS pacientes (
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   nome TEXT NOT NULL,
+                   idade INTEGER,
+                   telefone TEXT
+                   )
+                   """)
+    conexao.commit()
+    conexao.close()
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from database.conexao import conectar
+from database.conexao import conectar, criar_tabela_pacientes
 
 conexao = conectar()
 
+criar_tabela_pacientes()
+
 print("🏥 MedFlow conectado ao banco com sucesso!")
+print("📋 Tabela de pacientes criada com sucesso!")
